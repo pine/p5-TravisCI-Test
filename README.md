12f4c5ef6ff995bcb25a9eeaef8677a3eaabc30f
Pine Mizune <pinemz@gmail.com>
2018-01-02 18:26:58 +0900

    feat(repository): initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+TravisCI::Test - It's new $module
+
+# SYNOPSIS
+
+    use TravisCI::Test;
+
+# DESCRIPTION
+
+TravisCI::Test is ...
+
+# LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Pine Mizune <pinemz@gmail.com>
---
+++ lib/TravisCI/Test.pm
@@ -0,0 +1,39 @@
+package TravisCI::Test;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+TravisCI::Test - It's new $module
+
+=head1 SYNOPSIS
+
+    use TravisCI::Test;
+
+=head1 DESCRIPTION
+
+TravisCI::Test is ...
+
+=head1 LICENSE
+
+Copyright (C) Pine Mizune.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Pine Mizune E<lt>pinemz@gmail.comE<gt>
+
+=cut
+
